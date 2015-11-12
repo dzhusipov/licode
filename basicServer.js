249688805e8d15e4dc5e6b470238563229ce570a
dzhusipov <zh.daur@gmail.com>
2015-11-12 10:31:06 +0600

    2015-11-12--10:31:06

--- basicServer.js
+++ basicServer.js
@@ -44,16 +44,16 @@ var roomName;
 console.log(__dirname);
 
 app.get('/dasm/', function(req, res){
-    /*roomName = req.params.roomName; 
+    roomName = req.query.roomName; 
 
     if (roomName == ""){
         roomName = "testroom";
-    }*/
+    }
     res.sendFile(__dirname + '/server/room.html', {
         isMain: true
     });
 
-  console.log(req.params);
+  console.log(roomName);
 });
 
 app.get('/asdasdasd/:roomName', function(req, res) {
