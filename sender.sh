e5666e171b92f967afb909750731b4407cfdae0c
dzhusipov <zh.daur@gmail.com>
2015-11-18 17:08:41 +0600

    2015-11-18--17:08:41

--- sender.sh
+++ sender.sh
@@ -31,19 +31,19 @@ do
 		do
 			((increment++))
 			iin=${file:0:${#file} - 3}
-			echo "	find iin is: $iin" >> $LOFGILE	
+			echo "	finded iin is: $iin" >> $LOFGILE	
 			if [[ $increment = "1" ]]
 			then 
 				file1=$(echo $line | awk {'print $4'})
 				echo "	sending first file : $file1" >> $LOFGILE	
-				RESULTOFREST=`curl -F "File=@$file1" -F "DocumentType=VEREF" -H "Role:Client" -H "IIN:$iin" -H "Content-Type:multipart/form-data" --request POST http://192.168.15.3:9082/ecmapi/json/documents?DocumentType=VEREF`
+				RESULTOFREST=`curl -F "File=@$file1" -F "DocumentType=VEREF" -H "Role:Client" -H "IIN:$iin" -H "Content-Type:multipart/form-data" --request POST http://192.168.15.3:9082/ecmapi/json/documents?DocumentType=VEREF >> dasm.log`
 				echo "	Send result of first file: $RESULTOFREST" >> $LOFGILE
 			fi
 			if [[ $increment = "2" ]]
 			then 
 				file2=$(echo $line | awk {'print $4'})
 				echo "	sending second one : $file2" >> $LOFGILE
-				RESULTOFREST=`curl -F "File=@$file2" -F "DocumentType=VEREF" -H "Role:Client" -H "IIN:$iin" -H "Content-Type:multipart/form-data" --request POST http://192.168.15.3:9082/ecmapi/json/documents?DocumentType=VEREF`
+				RESULTOFREST=`curl -F "File=@$file2" -F "DocumentType=VEREF" -H "Role:Client" -H "IIN:$iin" -H "Content-Type:multipart/form-data" --request POST http://192.168.15.3:9082/ecmapi/json/documents?DocumentType=VEREF >> dasm.log`
 				echo "	Send result of second file: $RESULTOFREST" >> $LOFGILE
 			fi
 		 	#echo $increment
@@ -54,6 +54,8 @@ do
 		#rm $file1
 		#rm $file2
 		#rm $file
+		
+	else
 		echo "finished" >> $file
 	fi
 	echo "	---------- $file not finished" >> $LOFGILE	
