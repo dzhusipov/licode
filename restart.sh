4b8347e373145f89750e270c281db6bbed42e8da
dzhusipov <zh.daur@gmail.com>
2015-11-24 08:21:02 +0600

    2015-11-24--08:21:02

---
+++ restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Licode was cloned under /var/lib in our case
+cd /home/dasm/git
+# This kills all the node.js services running.  BEWARE of this command
+# if you have other node services running
+killall node
+
+./licode/scripts/initLicode.sh #> licode.log
+./licode/scripts/initBasicExample.sh #> licode_basic.log
+
+exit 0
