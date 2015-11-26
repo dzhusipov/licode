f6934f5711f4a9f19c2a245a77f54695a823be00
dzhusipov <zh.daur@gmail.com>
2015-11-26 11:04:25 +0600

    2015-11-26--11:04:25

--- basicServer.js
+++ basicServer.js
@@ -77,9 +77,9 @@ app.get('/dasm/', function(req, res){
     /*
         cookies mazafaka nah
     */
-    //var params = req.query.split('&')[1];
-    console.log('************************req.query.roomName = ' + req.query.iin);
-    /*
+    var iin = req.query.iin
+
+    
     if (!req.cookies.iin) {
         var hour = 60 * 60 * 1000;
         console.log("in IIN MAZAHAKA NAH");
@@ -89,7 +89,7 @@ app.get('/dasm/', function(req, res){
         countcookie = req.cookies.count+1;
         res.cookie('count', countcookie, { maxAge: hour });
     }
-    */
+    
     //--------------------------------------------------
 
     roomName = req.query.roomName;  //UUID
