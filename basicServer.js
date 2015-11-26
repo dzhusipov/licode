5f303080c60ac2f8e5b8b398faf88929d4d6fa4c
dzhusipov <zh.daur@gmail.com>
2015-11-26 11:05:29 +0600

    2015-11-26--11:05:29

--- basicServer.js
+++ basicServer.js
@@ -80,7 +80,7 @@ app.get('/dasm/', function(req, res){
     var iin = req.query.iin
 
     
-    if (!req.cookies.iin) {
+    if (req.cookies.iin == undefined) {
         var hour = 60 * 60 * 1000;
         console.log("in IIN MAZAHAKA NAH");
         res.cookie('iin', iin, { maxAge: hour });
