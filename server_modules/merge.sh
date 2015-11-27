f7cfd11aaefdcf2084198b0166a8aa02b2e1a43e
dzhusipov <zh.daur@gmail.com>
2015-11-27 14:07:19 +0600

    2015-11-27--14:07:19

--- server_modules/merge.sh
+++ server_modules/merge.sh
@@ -12,7 +12,7 @@ LOGFILE=$ROOTPATH$DATENOWIS$LOGNAME
 NOW=$(date +"%Y-%m-%d %T") 
 echo "$NOW ------------------------ Merging started ------------------------ " >> $LOGFILE
 
-folders=$(ls | grep lalalala) # change lalala to normal output
+folders=$(ls -l | grep '^d' | awk '{ print $9 }') # change lalala to normal output |grep -o '[0-9]*'
 
 #check each folder
 for folder in $folders
