03c5e76c0206eb24ecff3228f5a649c95aa63b50
dzhusipov <zh.daur@gmail.com>
2015-12-02 15:42:21 +0600

    2015-12-02--15:42:21

--- public/script.js
+++ public/script.js
@@ -63,7 +63,8 @@ window.onload = function () {
       optional: [ ]
   };
   var screen = getParameterByName("screen");
-  var config = {audio: true, video: video_constraints, data: true, screen: screen, videoSize: [640, 480, 640, 480]};
+  //var config = {audio: true, video: video_constraints, data: true, screen: screen, videoSize: [640, 480, 640, 480]};
+  var config = {audio: true, video: video_constraints, data: true, screen: screen, videoSize: [16, 9, 16, 9]};
   // If we want screen sharing we have to put our Chrome extension id. The default one only works in our Lynckia test servers.
   // If we are not using chrome, the creation of the stream will fail regardless.
   if (screen){
