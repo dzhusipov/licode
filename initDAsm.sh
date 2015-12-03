61e112653c7903d06bd803305ada86076522bca7
dzhusipov <zh.daur@gmail.com>
2015-12-03 09:10:46 +0600

    2015-12-03--09:10:46

--- initDAsm.sh
+++ initDAsm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 clear
-cat ./server_modules/logo.dat
-cp server_modules/erizoController.js ../../erizo_controller/erizoController/erizoController.js 
+cat /home/dasm/git/licode/extras/basic_example/server_modules/logo.dat
+cp /home/dasm/git/licode/extras/basic_example/server_modules/erizoController.js /home/dasm/git/licode/erizo_controller/erizoController/erizoController.js 
 git pull && bash ../../../restart.sh
 echo "----------------------------------done---------------------------------------"
