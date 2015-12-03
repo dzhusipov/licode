3876ed676233ce25851df8bce28944a7116c3602
dzhusipov <zh.daur@gmail.com>
2015-12-03 10:29:57 +0600

    2015-12-03--10:29:57

--- public/video_config.js
+++ public/video_config.js
@@ -1,4 +1,12 @@
+/*
+	Licode config on recording
+	you can use only 4:3 resolution
+	for example:
+	640x480
+	320x240
+*/
+
 var video_width = [
-  320,  // video width on recording
-  240   // video heigth on recording
+  640,  // video width on recording
+  480   // video heigth on recording
 ];
